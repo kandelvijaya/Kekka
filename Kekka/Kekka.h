40f8b042c5153e01d6b29a6be9ade292ab5cce9e
vkandel <vijaya.prakash.kandel@zalando.de>
2018-01-08 21:13:26 +0100

    Initial Commit

---
+++ Kekka/Kekka.h
@@ -0,0 +1,19 @@
+//
+//  Kekka.h
+//  Kekka
+//
+//  Created by Vijaya Prakash Kandel on 08.01.18.
+//  Copyright © 2018 com.kandelvijaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kekka.
+FOUNDATION_EXPORT double KekkaVersionNumber;
+
+//! Project version string for Kekka.
+FOUNDATION_EXPORT const unsigned char KekkaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kekka/PublicHeader.h>
+
+
